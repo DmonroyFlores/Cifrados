97ec0a1eca6ebb8787fa4788fc70d47e2ae1e48d
DmonroyFlores <dmonroyf2000alumno.ipn>
2023-10-21 21:22:16 -0600

    index.html

---
+++ js/Menu.js
@@ -0,0 +1,13 @@
+function Mostrar_cesar(){
+    document.getElementById('Menu_cesar').style.display='block'
+    document.getElementById('menu').style.display= 'none';
+}
+function Mostrar_viggenere(){
+    document.getElementById('Menu_viggenere').style.display = 'block';
+    document.getElementById('menu').style.display= 'none';
+}
+function Mostrar_menu(){
+    document.getElementById('Menu_viggenere').style.display = 'none';
+    document.getElementById('Menu_cesar').style.display='none'
+    document.getElementById('menu').style.display= 'block';
+}
